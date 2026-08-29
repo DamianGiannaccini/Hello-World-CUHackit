d8ea4189eb7fa4e2f9f7360ac84b43561407590f
Pil0tPants <dman2k5@gmail.com>
2026-08-29 14:17:13 -0400

    User Attack Input (WIP)

--- cu-hackit-hello-world/UIController.gd
+++ cu-hackit-hello-world/UIController.gd
@@ -1,27 +1,30 @@
 extends Control
 
-
+@export var question_list : Array[Array]
 
 
 func on_text_entered() -> void:
-	var text : String = $TextEdit.get_line(0)
+	var text : String = $TextInput.get_line(0)
 	text = text.strip_edges()
 	
 	if text.nocasecmp_to("Attack()") == 0:
-		print("Attack")
+		prompt_question(0)
 	elif text.nocasecmp_to("Crouch()") == 0:
 		print("Crouch")
 	elif text.nocasecmp_to("Block()") == 0:
 		print("Block")
 	
-	$TextEdit.text = ""
+	$TextInput.text = ""
 
+func prompt_question(index : int):
+	$QuestionLabel.text = question_list[index][0]
+	$QuestionLabel.visible = true
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("Enter"):
 		on_text_entered()
 	elif(Input.is_action_just_pressed("InputBar")):
-		if $TextEdit.visible:
-			$TextEdit.visible = false
+		if $TextInput.visible:
+			$TextInput.visible = false
 		else:
-			$TextEdit.visible = true
+			$TextInput.visible = true
